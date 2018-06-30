6abed4f83d3228fefe4d5426eb608db3fe0b25a3
Evg <budo@narod.ru>
2018-06-30 08:36:40 +0300

    Create donate_controller.rb

---
+++ app/controllers/discourse_donate/donate_controller.rb
@@ -0,0 +1,19 @@
+module DiscourseDonate
+	
+	class DonateController < ApplicationController
+  skip_before_action :check_xhr, only: [:index]
+
+  def index
+    @dev = About.new
+    respond_to do |format|
+      format.html do
+        render :index
+      end
+     format.json do
+       render_serialized(@dev, AboutSerializer)
+     end
+    end
+  end
+
+end
+end
