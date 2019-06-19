141a4945f6401a5531b28ac3ec0d8859b461cfce
Evg <budo@narod.ru>
2019-06-19 11:21:10 +0300

    Update engine.rb

--- lib/dev/engine.rb
+++ lib/dev/engine.rb
@@ -47,17 +47,6 @@ module DiscourseHelp
   end
 end
 
-module DiscourseStats
-  class Engine < ::Rails::Engine
-    isolate_namespace DiscourseStats
-
-    config.after_initialize do
-		Discourse::Application.routes.append do
-			mount ::DiscourseStats::Engine, at: "/"
-		end
-    end
-  end
-end
 
 module DiscourseLevel
   class Engine < ::Rails::Engine
